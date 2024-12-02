a77f4ac9b444bc308fa66eb9eadaaef9bcafca48
Shane M <ShaneMorgann@gmail.com>
2024-12-02 05:39:18 -0500

    added constants.hh with namespace for configuration

---
+++ isometric-tiling-example/include/constants.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+namespace config{
+
+    // window config
+    constexpr int WINDOW_WIDTH = 1920;
+    constexpr int WINDOW_HEIGHT = 1080;
+    const std::string WINDOW_TITLE = ("Isometric-Tiling-Example");
+
+    // game logic
+    constexpr int TARGET_FPS = 120;
+
+    // map dimensions
+    constexpr int MAP_WIDTH = 10;
+    constexpr int MAP_HEIGHT = 10;
+
+}
--- isometric-tiling-example/src/main.cc
+++ isometric-tiling-example/src/main.cc
@@ -1,6 +1,7 @@
 #include "../../.raylib/raylib.h"
 
 #include "../include/spritesheet.hh"
+#include "../include/constants.hh"
 
 int main(){
     
