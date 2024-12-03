7ce0cccd33d4fe54851e927ffe0295dae75b8c79
Shane M <ShaneMorgann@gmail.com>
2024-12-03 08:28:21 -0500

    added Window struct

---
+++ isometric-tiling-example/include/window.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "constants.hh"
+#include <raylib.h>
+
+struct Window{
+    static constexpr int width = config::WINDOW_WIDTH;
+    static constexpr int height = config::WINDOW_HEIGHT;
+    static constexpr const char* title = config::WINDOW_TITLE;
+
+    static constexpr int targetFPS = config::TARGET_FPS;
+
+    static constexpr Vector2 windowCenterPoint = {width / 2.0f, height / 2.0f};
+};
