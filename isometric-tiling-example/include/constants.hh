86d3b76eadfc6e4b4887926924356bfefbb8ac29
Shane M <ShaneMorgann@gmail.com>
2024-12-03 09:13:07 -0500

    updated constants.hh with more values

--- isometric-tiling-example/include/constants.hh
+++ isometric-tiling-example/include/constants.hh
@@ -12,12 +12,23 @@ namespace config{
 
     // game logic
     constexpr int TARGET_FPS = 120;
-    constexpr float SPRITE_SCALE = 3;
 
     // map dimensions
     constexpr int MAP_WIDTH = 15;
     constexpr int MAP_HEIGHT = 15;
 
+    // sprite properties
     constexpr int SPRITE_SIZE = 32;
+    constexpr float SPRITE_SCALE = 3;
+
+    // debug
+    constexpr bool ENABLE_FPS_UI = true;
+    constexpr int FPS_POSITION_X = 5;
+    constexpr int FPS_POSITION_Y = 5;
+
+    constexpr bool ENABLE_DEBUG_UI = false;
+    constexpr float DEBUG_LINE_THICKNESS = 3.0f;
+    constexpr Color DEBUG_LINE_COLOR = MAGENTA;
+
 
 }
