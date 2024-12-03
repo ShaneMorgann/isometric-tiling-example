9f761fe4e6f1723093f4c400b6731a7ecb1f070f
Shane M <ShaneMorgann@gmail.com>
2024-12-03 09:12:36 -0500

    rendering implementation into main.cc

--- isometric-tiling-example/src/main.cc
+++ isometric-tiling-example/src/main.cc
@@ -1,20 +1,49 @@
 #include "../../.raylib/raylib.h"
 
+#include "../include/map.hh"
+#include "../include/window.hh"
 #include "../include/spritesheet.hh"
-#include "../include/constants.hh"
-#include <raylib.h>
+#include "../include/block.hh"
+#include "../include/render.hh"
+
+
+constexpr Map map;
+constexpr Window window;
+Spritesheet spritesheet("../data/isometric-spritesheet.png", config::SPRITE_SIZE);
 
 int main(){
 
-    InitWindow(config::WINDOW_WIDTH, config::WINDOW_HEIGHT, config::WINDOW_TITLE.c_str());
-    SetTargetFPS(config::TARGET_FPS);
+    InitWindow(window.width, window.height, window.title);
+    SetTargetFPS(window.targetFPS);
+
+    Block block{
+        spritesheet.selectSpriteUniformSpacing(0, 0),
+        spritesheet.spriteSize
+    };
 
     while (!WindowShouldClose()){
+        
         BeginDrawing();
         ClearBackground(config::BG_CLEAR_COLOR);
+
+        for(int x = 0; x < map.width; x++){
+            for(int y = 0; y < map.height; y++){
+                render::drawBlock(&window, &block, &map, Vector2i{x,y});
+            }
+        }
+
+        // debug ui
+        if(config::ENABLE_FPS_UI){
+            DrawFPS(config::FPS_POSITION_X, config::FPS_POSITION_Y);
+        }
+        if(config::ENABLE_DEBUG_UI){
+            DrawLineEx({window.windowCenterPoint.x, 0}, {window.windowCenterPoint.x, window.height}, config::DEBUG_LINE_THICKNESS, config::DEBUG_LINE_COLOR);
+            DrawLineEx({0, window.windowCenterPoint.y}, {window.width, window.windowCenterPoint.y}, config::DEBUG_LINE_THICKNESS, config::DEBUG_LINE_COLOR);
+        }
 
         EndDrawing();
     }
 
+    UnloadTexture(block.texture);
     return 0;
 }
