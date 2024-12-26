064010da0022632775d91a748785648dd6e6490b
Shane M <ShaneMorgann@gmail.com>
2024-12-26 07:25:55 -0500

    removed uneeded variables that are no pulled from constants.hh

--- isometric-tiling-example/include/block.hh
+++ isometric-tiling-example/include/block.hh
@@ -1,13 +1,7 @@
 #pragma once
 
 #include "raylib.h"
-#include "constants.hh"
 
 struct Block{
     Texture2D texture;
-    float size;
-    float sizeScale = size * config::SPRITE_SCALE;
-    float sizeHalfScale = sizeScale * 0.5;
-
-    Vector2 sizeHalfScaleVector{sizeHalfScale, sizeHalfScale};
 };
